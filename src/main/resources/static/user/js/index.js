b425dcf678ec927f333d5346aa0e9a3ebb3a6ec7
vienmv <vienmv@DESKTOP-K9QCC46>
2019-03-03 06:24:05 +0700

    index html

---
+++ src/main/resources/static/user/js/index.js
@@ -0,0 +1,30 @@
+<link href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i" rel="stylesheet" type="text/css">
+<script src="http://code.jquery.com/jquery-1.9.1.js" type="text/javascript"></script>
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js" type="text/javascript"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+<script language="javascript">
+  function clear_searchBox() {
+    if (document.getElementById('search_box').value == "search library website...")
+        document.getElementById('search_box').value = "";
+  }         
+</script>
+
+<script src="https://support.ebscohost.com/eit/scripts/ebscohostsearch.js" type="text/javascript"></script>     
+
+<!-- begin SnapEngage code --><script type="text/javascript">
+  (function() {
+    var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
+    se.src = '//storage.googleapis.com/code.snapengage.com/js/f33cf00f-c98a-4484-a8d8-2b1c4dc6ffba.js';
+    var done = false;
+    se.onload = se.onreadystatechange = function() {
+      if (!done&&(!this.readyState||this.readyState==='loaded'||this.readyState==='complete')) {
+        done = true;
+        /* Place your SnapEngage JS API code below */
+        /* SnapEngage.allowChatSound(true); Example JS API: Enable sounds for Visitors. */
+      }
+    };
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+  })();
+</script><!-- end SnapEngage code -->
+	<script type="text/javascript" src="//libapps.s3.amazonaws.com/sites/494/include/ebscoSearch.js"></script>
---
+++ src/test/java/com/vienmv/fptLibrary/FptLibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vienmv.fptLibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FptLibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
